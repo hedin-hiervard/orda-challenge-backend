fe6b57e88f47e4282885ba29ab64009383caa2dd
hedin <flame.beholder@gmail.com>
2019-05-16 11:33:46 +0300

    implemented GraphQL server able to query database and receive sendReport command

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
+import '@babel/polyfill'
 import Youch from 'youch'
 import forTerminal from 'youch-terminal'
 import { StreamLogger } from 'ual'
+import dotenv from 'dotenv'
+
+import Server from 'server'
 
 export const log = new StreamLogger({ stream: process.stdout, colors: true })
+
+dotenv.config()
 
 process.on('unhandledRejection', err => {
     throw err
@@ -15,6 +21,19 @@ process.on('uncaughtException', err => {
         .then((output) => {
             log.error(forTerminal(output))
         })
-})
+});
 
-log.debug('started')
+(async function() {
+    if(process.env.BACKEND_PORT == null) {
+        throw new Error('BACKEND_PORT env var is undefined')
+    }
+    if(process.env.DATA_FILE == null) {
+        throw new Error('DATA_FILE env var is undefined')
+    }
+    const server = new Server({
+        port: process.env.BACKEND_PORT,
+        log,
+        dataFile: process.env.DATA_FILE,
+    })
+    await server.start()
+})()
---
+++ src/server.js
@@ -0,0 +1,125 @@
+import fs from 'fs-extra'
+import graphqlHTTP from 'express-graphql'
+import express from 'express'
+import { makeExecutableSchema } from 'graphql-tools'
+import _ from 'lodash'
+import moment from 'moment'
+
+const typeDefs = `
+    type Venue {
+        id: String!
+        name: String!
+    }
+
+    type Result {
+        success: Boolean!,
+        msg: String
+    }
+
+    type Order {
+        time: String!
+        tipSum: Float!
+        sumTotal: Float!
+        venueId: String!
+        venueName: String!
+    }
+
+    type Day {
+        start: String!
+        desc: String!
+    }
+
+    type Query {
+        days: [ Day ]
+        venueNames: [ Venue ]
+        orders(venueId: String): [ Order ]
+    }
+    type Mutation {
+        sendReport(
+            email: String!
+            dayStart: String!
+            venueId: String!
+        ): Result!
+    }
+`
+export default class Server {
+    constructor({ port, log, dataFile }) {
+        this.port = port
+        this.log = log
+        this.dataFile = dataFile
+    }
+
+    async start() {
+        this.log.info(`reading dataFile: ${this.dataFile}`)
+        this.data = fs.readFileSync(this.dataFile, 'utf-8')
+        this.data = JSON.parse(this.data)
+        if(!Array.isArray(this.data)) {
+            throw new Error(`corrupt data.json, array expected as root element`)
+        }
+        this.log.info(`successfully read ${this.data.length} entries from data file`)
+
+        const resolvers = {
+            Mutation: {
+                sendReport: (obj, args) => {
+                    return {
+                        success: true,
+                        msg: 'no msg',
+                    }
+                },
+            },
+            Query: {
+                days: (obj) => {
+                    let days = this.data
+                        .map(order => moment.unix(order.time).startOf('day'))
+                    days = days
+                        .map(m => ({
+                            start: m.valueOf(),
+                            desc: m.format(),
+                        }))
+                    return days
+                },
+                venueNames: () => {
+                    let result = this.data
+                    result = _.uniqBy(result, order => order.venueId)
+                    result = result
+                        .map(order => ({
+                            name: order.venueName.venue,
+                            id: order.venueId,
+                        })
+                        )
+                    return result
+                },
+                orders: (obj, args) => {
+                    let result = this.data
+                    if(args.venueId != null) {
+                        result = result
+                            .filter(order => order.venueId === args.venueId)
+                    }
+                    result = result.map(
+                        order => ({
+                            ...order,
+                            venueName: order.venueName.venue,
+                        }))
+                    return result
+                },
+            },
+        }
+        const schema = makeExecutableSchema({
+            typeDefs,
+            resolvers,
+        })
+
+        this.app = express()
+        this.app.use('/',
+            graphqlHTTP((req, res) => ({
+                schema,
+                graphiql: true,
+                context: { req, res },
+            }))
+        )
+
+        this.log.info(`starting backend server on port ${this.port}`)
+        await this.app.listen(this.port)
+        this.log.info(`server started`)
+    }
+}
